72fd6c19410c10356adb77e6214e5e8d5aac3d80
Michael-Longley <84longleym@gmail.com>
2018-04-29 21:42:13 -0400

    Routing working between the three pages.

---
+++ src/components/add_question.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class add_question extends Component{
+    render(){
+        return(
+            <div>
+                <form>
+                    give me all your questions
+                </form>
+            </div>
+        )
+    }
+}
+
+export default add_question;
---
+++ src/components/review.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class review extends Component{
+    render(){
+        return(
+            <div>
+                <form>
+                    Review some stuff
+                </form>
+            </div>
+        )
+    }
+}
+
+export default review;
