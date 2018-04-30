8acf55e008cd356e5290de3bd7cdcd797edcd309
Michael-Longley <84longleym@gmail.com>
2018-04-29 22:02:12 -0400

    Created basic form for exam question.

--- src/components/add_question.jsx
+++ src/components/add_question.jsx
@@ -5,7 +5,33 @@ class add_question extends Component{
         return(
             <div>
                 <form>
-                    give me all your questions
+                    Quesion :  
+                    <input type='text'/>
+                    <br/>
+                    <br/>
+                    Answer : 
+                    <input type='text'/>
+                    <br/>
+                    <br/>
+                    Type : 
+                    <select>
+                        <option value=''>Select One</option>
+                        <option value='multiple_choice'>Multiple Choice</option>
+                        <option value='matching'>Matching</option>
+                        <option value='pseudo_code'>Pseudo Code</option>
+                        <option value='short_answer'>Short Answer</option>
+                        <option value='long_answer'>Long Answer</option>
+                        <option value='multiple_choice'>Multiple Choice</option>
+                    </select>
+                    {/* <input type='select'/> */}
+                    <br/>
+                    <br/>
+                    Tags : 
+                    <input type='text'/>
+                    <br/>
+                    <br/>
+                    <button>Cancel</button>
+                    <button>Submit</button>
                 </form>
             </div>
         )
